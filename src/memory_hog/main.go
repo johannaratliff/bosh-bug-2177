061a3a63426bafa92dd0c9e3f53554393c12265a
Johanna Ratliff <josmith@pivotal.io>
2019-06-25 12:31:02 -0600

    Officially a working release

--- src/memory_hog/main.go
+++ src/memory_hog/main.go
@@ -6,19 +6,24 @@ import (
 	"time"
 )
 
-var sha string
+var SHA string
 
 func main() {
 	loggr := log.New(os.Stderr, "", log.LUTC)
 
 	loggr.Println("Starting memory hog process...")
-	loggr.Printf("SHA: %s \n", sha)
+	loggr.Println(time.UnixDate)
+	loggr.Printf("SHA: %s \n", SHA)
 
-	go func() {
-		for {
-			var slice []int64
-			time.Sleep(100 * time.Millisecond) // make sure the CPU isn't overloaded
-			slice = append(slice, time.Now().UnixNano())
-		}
-	}()
+	for {
+		loggr.Println("Starting eating up memory")
+		time.Sleep(100 * time.Millisecond) // make sure the CPU isn't overloaded
+		go func() {
+			for {
+				var slice []int64
+				time.Sleep(100 * time.Millisecond) // make sure the CPU isn't overloaded
+				slice = append(slice, time.Now().UnixNano())
+			}
+		}()
+	}
 }
